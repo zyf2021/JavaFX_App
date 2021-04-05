b148f7150fd6d3b766224dbc49c2b61d641bd94a
Zyf <freelance2020@bk.ru>
2021-04-05 19:46:57 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,76 @@
+package sample;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Controller {
+    @FXML
+    Button donateBtn;
+
+    @FXML
+    Button runnerBtn;
+
+    @FXML
+    Button moreInfoBtn;
+
+    public void DonateWindowOpen(javafx.event.ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("donatePage.fxml"));
+            Scene scene = donateBtn.getScene();
+            scene.setRoot(root);
+        }
+        catch(IOException e) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE, "Failed to create new DonateWindow.", e);
+        }
+    }
+
+    public void RunnerWindowOpen(ActionEvent actionEvent) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("runnerPage.fxml"));
+            Scene scene = runnerBtn.getScene();
+            scene.setRoot(root);
+        }
+        catch(IOException e) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE, "Failed to create new RunnerWindow.", e);
+        }
+    }
+    public void MoreInfoWindowOpen(ActionEvent actionEvent) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("moreInfoPage.fxml"));
+            Scene scene = moreInfoBtn.getScene();
+            scene.setRoot(root);
+        }
+        catch(IOException e) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE, "Failed to create new InfoWindow.", e);
+        }
+    }
+//    public void CreateNewWindow(ActionEvent actionEvent, String namePage, String titlePage){
+//        try{
+//            FXMLLoader fxmlLoader = new FXMLLoader();
+//            fxmlLoader.setLocation(getClass().getResource(namePage));
+//            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+//            Stage donateWindow = new Stage();
+//            donateWindow.setTitle(titlePage);
+//            donateWindow.setScene(scene);
+//            donateWindow.show();
+//        }
+//        catch(IOException e) {
+//            Logger logger = Logger.getLogger(getClass().getName());
+//            logger.log(Level.SEVERE, "Failed to create new Window.", e);
+//        }
+//    }
+}
+
---
+++ src/sample/MoreInfoPage.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MoreInfoPage {
+    @FXML
+    Button backBtn;
+
+    public void MainWindowOpen(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            Scene scene = backBtn.getScene();
+            scene.setRoot(root);
+        }
+        catch(IOException e) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE, "Failed to create new InfoWindow.", e);
+        }
+    }
+}
